ecb3262d194123d8258c00584768d4f1405ad389
Nick Zakharov <46914559+Nick-02@users.noreply.github.com>
2019-03-12 06:33:56 +0200

    Програма керування об'єктами

---
+++ Risovanie/RisovanieDlg.h
@@ -0,0 +1,33 @@
+// RisovanieDlg.h : header file
+//
+
+#pragma once
+
+
+// CRisovanieDlg dialog
+class CRisovanieDlg : public CDialog
+{
+// Construction
+public:
+	CRisovanieDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_RISOVANIE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
